1631dcca98dc239ba115341b692d1fda39cd750c
inekit <nicklzx@uni-dubna.ru>
2022-07-09 07:45:35 +0300

    fixed some sheet

--- Utils/statistics.js
+++ Utils/statistics.js
@@ -4,8 +4,8 @@ class Statistics {
     
     async increaseCart(userId){
         (await tOrmCon).query(
-            `INSERT INTO shop.statistics (date,users_per_day,cart_per_day) 
-            SELECT CAST(now() AS DATE), 0,0 FROM shop.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = 1 LIMIT 1
+            `INSERT INTO channels.statistics (date,users_per_day,cart_per_day) 
+            SELECT CAST(now() AS DATE), 0,0 FROM channels.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = 1 LIMIT 1
             ON DUPLICATE KEY UPDATE cart_per_day = cart_per_day+1`, 
             [userId])
         .catch((e)=>{
@@ -16,8 +16,8 @@ class Statistics {
 
     async increaseUse(userId){
         (await tOrmCon).query(
-            `INSERT INTO shop.statistics (date,users_per_day,cart_per_day) 
-            SELECT CAST(now() AS DATE), 0,0 FROM shop.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = ? LIMIT 1
+            `INSERT INTO channels.statistics (date,users_per_day,cart_per_day) 
+            SELECT CAST(now() AS DATE), 0,0 FROM channels.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = ? LIMIT 1
             ON DUPLICATE KEY UPDATE users_per_day = users_per_day+1`, 
             [userId])
         .catch((e)=>{
--- scenes/adminScene.js
+++ scenes/adminScene.js
@@ -46,9 +46,9 @@ function formatDate(date) {
 adminScene.hears(titles.getValues('BUTTON_STATISTICS'),async ctx => {
     const connection =await tOrmCon
     connection.query(`SELECT date, users_per_day, cart_per_day, users_per_week, cart_per_week from 
-	(SELECT date, users_per_day, cart_per_day FROM shop.statistics
+	(SELECT date, users_per_day, cart_per_day FROM channels.statistics
 	WHERE DATEDIFF(now(), date) < 7) day,
-    (SELECT sum(users_per_day) users_per_week, sum(cart_per_day) cart_per_week FROM shop.statistics
+    (SELECT sum(users_per_day) users_per_week, sum(cart_per_day) cart_per_week FROM channels.statistics
 	WHERE DATEDIFF(now(), date) < 7 GROUP BY date) week`)
     .then((res) => {
         if (!res || !res.length)  return ctx.replyWithTitle('THERE_IS_NO_STAT')
